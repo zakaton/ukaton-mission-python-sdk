3e0d9c054de48772dc44ac6a3556758578bae59e
Zack <zack@ukaton.com>
2023-07-06 19:32:29 -0700

    progresss

--- examples/graph.py
+++ examples/graph.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append(".")
 
-from typing import Union
+from typing import Union, Tuple
 from dataclasses import dataclass
 
 import logging
@@ -11,6 +11,13 @@ logger.setLevel(logging.DEBUG)
 
 import asyncio
 import threading
+
+# I used ChatGPT for the matplotlib stuff
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+matplotlib.use("TkAgg")
 
 from UkatonMissionSDK import BLEUkatonMission, UDPUkatonMission, ConnectionEventType, SensorType, MotionDataType, PressureDataType, BLEUkatonMissions, MotionDataEventType, PressureDataEventType, SensorDataConfigurations, SensorDataType, EventDispatcher, SensorDataEventType
 
@@ -25,43 +32,20 @@ if use_ble:
 else:
     ukaton_mission = UDPUkatonMission()
 
-
-def on_connection():
-    logger.debug("connected callback triggered :)")
-
-
-def on_disconnection():
-    logger.debug("disconnected callback triggered :O")
-
-
-# I used ChatGPT for the matplotlib stuff
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
-matplotlib.use("TkAgg")
-
-# Number of rows and columns in the grid
-num_rows = 1
-num_cols = 2
-
-fig, axes = plt.subplots(num_rows, num_cols)
-
-lines = []
-
-# Initialize the lines for each subplot
-for ax in axes.flatten():
-    line1, = ax.plot([], [], label='Sin')
-    line2, = ax.plot([], [], label='Cos')
-    line3, = ax.plot([], [], label='Tan')
-    lines.append([line1, line2, line3])
-    ax.legend()
-
-# Data for the curves
-x = np.linspace(0, 10, 100)
-y1 = np.sin(x)
-y2 = np.cos(x)
-y3 = np.tan(x)
+sensor_data_configurations: SensorDataConfigurations = {
+    SensorType.MOTION: {
+        MotionDataType.QUATERNION: 20,
+        # MotionDataType.ACCELERATION: 20,
+    },
+    SensorType.PRESSURE: {
+        # PressureDataType.PRESSURE_SINGLE_BYTE: 20,
+    }
+}
+sensor_data_events: dict[SensorType, list[SensorDataEventType]] = {
+    # SensorType.MOTION: list(sensor_data_configurations[SensorType.MOTION].keys()),
+    SensorType.MOTION: [MotionDataEventType.QUATERNION],
+    SensorType.PRESSURE: [PressureDataEventType.CENTER_OF_MASS]
+}
 
 
 @dataclass
@@ -72,23 +56,50 @@ class Datum:
 
 N = 20
 data: dict[SensorType: dict[SensorDataEventType: list[Datum]]] = {}
+sensor_data_event_types: list[Tuple[SensorType, SensorDataEventType]] = []
+
+for sensor_type in sensor_data_events:
+    data[sensor_type] = {}
+    for sensor_data_event_type in sensor_data_events[sensor_type]:
+        data[sensor_type][sensor_data_event_type] = []
+        sensor_data_event_types.append((sensor_type, sensor_data_event_type))
+
+        def event_listener(value, timestamp, sensor_type=sensor_type, sensor_data_event_type=sensor_data_event_type): return add_data(
+            sensor_type, sensor_data_event_type, value, timestamp)
+        event_dispatcher: EventDispatcher = ukaton_mission.motion_data_event_dispatcher if sensor_type is SensorType.MOTION else ukaton_mission.pressure_data_event_dispatcher
+        event_dispatcher.add_event_listener(
+            sensor_data_event_type, event_listener)
+
+number_of_sensor_data_event_types = len(sensor_data_event_types)
+
+# Data for the curves
+x = np.linspace(0, 10, 100)
 
 
 def update_plot(frames):
     for i, ax in enumerate(axes.flatten()):
-        # Update the data for the curves
-        y1_data = y1 + np.random.normal(0, 5, len(x))
-        y2_data = y2 + np.random.normal(0, 0.1, len(x))
-        y3_data = y3 + np.random.normal(0, 0.1, len(x))
+        if i >= number_of_sensor_data_event_types:
+            break
 
-        # Update the lines with new data
-        lines[i][0].set_data(x, y1_data)
-        lines[i][1].set_data(x, y2_data)
-        lines[i][2].set_data(x, y3_data)
+        sensor_type, sensor_data_event_type = sensor_data_event_types[i]
+        # lines[i][2].set_data(x, y3_data)
+        _data = data[sensor_type][sensor_data_event_type]
+        match (sensor_type, sensor_data_event_type):
+            case (SensorType.MOTION, MotionDataEventType.ACCELERATION | MotionDataEventType.LINEAR_ACCELERATION | MotionDataEventType.GRAVITY | MotionDataEventType.MAGNETOMETER | MotionDataEventType.GRAVITY):
+                pass
+            case (SensorType.MOTION, MotionDataEventType.QUATERNION):
+                pass
+            case (SensorType.MOTION, MotionDataEventType.EULER):
+                pass
+            case (SensorType.PRESSURE, PressureDataEventType.CENTER_OF_MASS):
+                pass
+            case (SensorType.PRESSURE, PressureDataEventType.HEEL_TO_TOE):
+                pass
+            case (SensorType.PRESSURE, PressureDataEventType.MASS):
+                pass
+            case _:
+                pass
 
-        # Set the y-axis limits to fit the data
-        ax.set_ylim([np.min([y1_data, y2_data, y3_data]),
-                     np.max([y1_data, y2_data, y3_data])])
     return lines
 
 
@@ -99,45 +110,81 @@ def add_data(sensor_type: SensorType, sensor_data_event_type: SensorDataEventTyp
     _data: list[Datum] = data[sensor_type][sensor_data_event_type]
     _data.append(datum)
     _data = _data[-N:]
-    print(len(_data))
+
+
+# Number of rows and columns in the grid
+num_rows = 2
+num_cols = 2
+
+
+# fig, axes = plt.subplots(num_rows, num_cols, constrained_layout=True)
+fig, axes = plt.subplots(
+    number_of_sensor_data_event_types, constrained_layout=True)
+lines = []
+
+# Initialize the lines for each subplot
+for i, ax in enumerate(axes.flatten()):
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    if i >= number_of_sensor_data_event_types:
+        ax.set_axis_off()
+        continue
+
+    sensor_type, sensor_data_event_type = sensor_data_event_types[i]
+    sensor_data_event_type_name = sensor_data_event_type.name.lower()
+    print(f"sensor_data_event_type_name: {sensor_data_event_type_name}")
+    ax.set_title(sensor_data_event_type_name)
+
+    lines_to_append = []
+    match (sensor_type, sensor_data_event_type):
+        case (SensorType.MOTION, MotionDataEventType.ACCELERATION | MotionDataEventType.LINEAR_ACCELERATION | MotionDataEventType.GRAVITY | MotionDataEventType.MAGNETOMETER | MotionDataEventType.GRAVITY):
+            line_x, = ax.plot([], [], label='x')
+            line_y, = ax.plot([], [], label='y')
+            line_z, = ax.plot([], [], label='z')
+            lines_to_append += [line_x, line_y, line_z]
+            ax.set_ylim(-1, 1)
+        case (SensorType.MOTION, MotionDataEventType.QUATERNION):
+            line_w, = ax.plot([], [], label='w')
+            line_x, = ax.plot([], [], label='x')
+            line_y, = ax.plot([], [], label='y')
+            line_z, = ax.plot([], [], label='z')
+            lines_to_append += [line_w, line_x, line_y, line_z]
+            ax.set_ylim(-1, 1)
+        case (SensorType.MOTION, MotionDataEventType.EULER):
+            line_yaw, = ax.plot([], [], label='yaw')
+            line_pitch, = ax.plot([], [], label='pitch')
+            line_roll, = ax.plot([], [], label='roll')
+            lines_to_append += [line_yaw, line_pitch, line_roll]
+            ax.set_ylim(-1, 1)
+        case (SensorType.PRESSURE, PressureDataEventType.CENTER_OF_MASS):
+            line_x, = ax.plot([], [], label='x')
+            line_y, = ax.plot([], [], label='y')
+            lines_to_append += [line_x, line_y]
+            ax.set_ylim(-1, 1)
+        case (SensorType.PRESSURE, PressureDataEventType.HEEL_TO_TOE):
+            line_heel_to_toe, = ax.plot([], [], label='heel to toe')
+            lines_to_append += [line_heel_to_toe]
+            ax.set_ylim(0, 1)
+        case (SensorType.PRESSURE, PressureDataEventType.MASS):
+            line_mass, = ax.plot([], [], label='mass')
+            lines_to_append += [line_mass]
+            ax.set_ylim(0, 1)
+        case _:
+            pass
+    lines.append(lines_to_append)
+    ax.legend()
 
 
 async def main():
-    ukaton_mission.connection_event_dispatcher.add_event_listener(
-        ConnectionEventType.CONNECTED, on_connection)
-    ukaton_mission.connection_event_dispatcher.add_event_listener(
-        ConnectionEventType.DISCONNECTED, on_disconnection)
-
+    logger.debug("attempting to connect...")
     await ukaton_mission.connect(device_identifier)
+    if ukaton_mission.is_connected:
+        logger.debug("connected!")
 
-    logger.debug("enabling sensor data...")
-    sensor_data_configurations: SensorDataConfigurations = {
-        SensorType.MOTION: {
-            MotionDataType.QUATERNION: 20,
-            # MotionDataType.ACCELERATION: 20,
-        },
-        SensorType.PRESSURE: {
-            # PressureDataType.PRESSURE_SINGLE_BYTE: 20,
-        }
-    }
-    sensor_data_events: dict[SensorType, list[SensorDataEventType]] = {
-        # SensorType.MOTION: list(sensor_data_configurations[SensorType.MOTION].keys()),
-        SensorType.MOTION: [MotionDataEventType.QUATERNION],
-        SensorType.PRESSURE: [PressureDataEventType.CENTER_OF_MASS]
-    }
-    for sensor_type in sensor_data_events:
-        data[sensor_type] = {}
-        for sensor_data_event_type in sensor_data_events[sensor_type]:
-            data[sensor_type][sensor_data_event_type] = []
-
-            def event_listener(value, timestamp, sensor_type=sensor_type, sensor_data_event_type=sensor_data_event_type): return add_data(
-                sensor_type, sensor_data_event_type, value, timestamp)
-            event_dispatcher: EventDispatcher = ukaton_mission.motion_data_event_dispatcher if sensor_type is SensorType.MOTION else ukaton_mission.pressure_data_event_dispatcher
-            event_dispatcher.add_event_listener(
-                sensor_data_event_type, event_listener)
-
-    await ukaton_mission.set_sensor_data_configurations(sensor_data_configurations)
-    logger.debug("enabled sensor data!")
+        logger.debug("enabling sensor data...")
+        await ukaton_mission.set_sensor_data_configurations(sensor_data_configurations)
+        logger.debug("enabled sensor data!")
 
 
 def run_main():
